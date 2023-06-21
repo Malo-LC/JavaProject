d9efc8a4612dd02b6f5c3a0c06d4b46d3c33419d
Malo <malolc2001@gmail.com>
2023-06-21 11:03:18 +0200

    up

--- src/Main.java
+++ src/Main.java
@@ -134,11 +134,3 @@ public class Main {
     CatalogPrinter.printCatalog(catalog);
   }
 }
-
-//    Single Responsibility Principle (SRP): Each class has a single responsibility and encapsulates related functionality.
-//
-//    Low Coupling Principle: The Catalog class, which manages the themes, doesn't depend directly on the printing functionality. The printing responsibility is delegated to the CatalogPrinter class, reducing coupling between classes.
-//
-//    Creator Pattern: The Catalog class is responsible for creating and managing themes, adhering to the Creator pattern.
-//
-//    Information Expert Principle: The CatalogPrinter class is responsible for printing the catalog as it has the necessary information about the catalog's structure.
